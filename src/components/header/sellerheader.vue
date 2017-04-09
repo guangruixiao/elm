ef2434f4aa6837a48d6e9c5dc02f3bb6ea7ca1cd
xgr <1245420476@qq.com>
2017-04-09 17:57:29 +0800

    构建header组件
    就近维护原则，建立header文件夹
    把父组件接收数据传递给header组件
    建立header组件HTML骨架

---
+++ src/components/header/sellerheader.vue
@@ -0,0 +1,25 @@
+<template>
+     <div class="sellerheader">
+      <div class="main">
+        <img class="sellerimg" v-bind:src=sellerMessage.avatar alt="">
+        <div class="sellerinfo">
+          <div class="name">{{sellerMessage.name}}</div>
+          <div class="send">{{sellerMessage.description}}/{{sellerMessage.deliveryTime}}分钟送达</div>
+          <div class="supports" v-if=sellerMessage.supports>
+            <span>{{sellerMessage.supports[0].type}}</span>
+            <span>{{sellerMessage.supports[0].description}}</span>
+            <span>{{sellerMessage.supports.length}}</span>
+          </div>
+        </div>
+      </div>
+      <div class="bulletin">{{sellerMessage.bulletin}}</div>
+     </div>
+</template>
+<script>
+export default {
+  props:["sellerMessage"]
+
+}
+</script>
+<style>
+</style>
