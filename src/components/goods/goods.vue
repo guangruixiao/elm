cb1d8c54448458c1ea0c1648149ebfd0af067b66
xgr <1245420476@qq.com>
2017-04-13 17:58:11 +0800

    商品详情页构建
    搭建HTML骨架

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,13 @@
 <template>
-     <div>
-      商品页
+     <div class="goods">
+      <div class="menubar">
+        <ul class="menubar-list">
+          <li class="menubar-item" v-for="item in goodsinfo">
+            <span class="menubar-name">{{item.name}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="content"></div>
     </div>
 </template>
 <script>
@@ -24,10 +31,40 @@ export default {
          });
       }
     },
-
-
 }
 
 </script>
 <style  lang="scss" scoped>
+
+  .goods{
+    display:flex;
+    position:absolute;
+    top:174px;
+    bottom:50px;
+    width:100%;
+    overflow:hidden;
+    .menubar{
+      flex: 0 0 80px;
+      width:80px;
+      background:#f3f5f7;
+      .menubar-item{
+        display: table;
+        height: 54px;
+        width: 56px;
+        padding: 0 12px;
+        font-size:12px;
+        line-height:14px;
+        font-weight:200;
+        .menubar-name{
+          display: table-cell;
+          width: 56px;
+          vertical-align: middle;
+
+        }
+      }
+    }
+    .content{
+      flex:1;
+    }
+  }
 </style>
